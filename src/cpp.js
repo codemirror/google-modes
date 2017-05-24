53513a8698b086722410c9b9e24a0a4f925f16b3
Marijn Haverbeke <marijn@haverbeke.nl>
2017-05-24 17:41:26 +0200

    Try to color local variables

--- src/cpp.js
+++ src/cpp.js
@@ -1,9 +1,27 @@
 import * as cpp from "./cpp.mode"
 
+function storeLocal(context, name) {
+  for (let cx = context; cx; cx = cx.parent)
+    if (cx.name == "Block" || cx.name == "FunctionDef")
+      return (cx.locals || (cx.locals = [])).push(name)
+}
+function isLocal(context, name) {
+  for (let cx = context; cx; cx = cx.parent)
+    if (cx.locals && cx.locals.indexOf(name) > -1) return true
+  return false
+}
+
 class CppMode extends CodeMirror.GrammarMode {
   constructor(conf) {
     super(cpp)
     this.conf = conf
+  }
+
+  token(stream, state) {
+    let inner = super.token(stream, state)
+    if (inner == "def") storeLocal(state.context, stream.current())
+    else if (inner == "variable" && isLocal(state.context, stream.current())) inner = "variable-2"
+    return inner
   }
 
   indent(state) {
