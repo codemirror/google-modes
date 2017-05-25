f656a243b7147da4538e20acdc62715574e08f3f
Marijn Haverbeke <marijn@haverbeke.nl>
2017-05-25 17:39:28 +0200

    More tests, small grammar tweaks

---
+++ test/cpp/main.cpp
@@ -0,0 +1,34 @@
+[meta #include <iostream>]
+[meta #include <vector>]
+[keyword using] [keyword namespace] [variable std];
+
+[type int] [def main]() {
+  [comment // create a vector to store int]
+  [type vector]<[type int]> [def vec];
+  [type int] [def i];
+
+  [comment // display the original size of vec]
+  [variable cout] [operator <<] [string "vector size = "] [operator <<] [variable-2 vec].[property size]() [operator <<] [variable endl];
+
+  [comment // push 5 values into the vector]
+  [keyword for] ([variable-2 i] [operator =] [number 0]; [variable-2 i] [operator <] [number 5]; [variable-2 i][operator ++]) {
+    [variable-2 vec].[property push_back]([variable-2 i]);
+  }
+
+  [comment // display extended size of vec]
+  [variable cout] [operator <<] [string "extended vector size = "] [operator <<] [variable-2 vec].[property size]() [operator <<] [variable endl];
+
+  [comment // access 5 values from the vector]
+  [keyword for] ([variable-2 i] [operator =] [number 0]; [variable-2 i] [operator <] [number 5]; [variable-2 i][operator ++]) {
+    [variable cout] [operator <<] [string "value of vec [["] [operator <<] [variable-2 i] [operator <<] [string "]] = "] [operator <<] [variable-2 vec][[ [variable-2 i] ]] [operator <<] [variable endl];
+  }
+
+  [comment // use iterator to access the values]
+  [type vector]<[type int]>::[type iterator] [def v] [operator =] [variable-2 vec].[property begin]();
+  [keyword while] ([variable-2 v] [operator !=] [variable-2 vec].[property end]()) {
+    [variable cout] [operator <<] [string "value of v = "] [operator <<] [operator *][variable-2 v] [operator <<] [variable endl];
+    [variable-2 v][operator ++];
+  }
+
+  [keyword return] [number 0];
+}
