f6cbc79cac0851bf17776232edf16da09f144453
Marijn Haverbeke <marijn@haverbeke.nl>
2017-08-26 22:29:38 +0200

    [cpp mode] Parse template instantions in expression position
    Closes #17

---
+++ test/cpp/grammar.cpp
@@ -0,0 +1,8 @@
+[meta foo::][qualified&callee&variable bar]();
+
+[type void] [meta X::][def Y]() {
+  [variable x_]->[callee&property Y]([variable&callee A]([keyword this], [operator &][meta B::][qualified&variable C]<[type D]>));
+}
+
+[keyword class] [def Z] {
+}
