d10331e08efbb90a8499778587cdf073ee733f44
Marijn Haverbeke <marijn@haverbeke.nl>
2017-08-28 22:50:02 +0200

    [cpp mode] Support [] after delete/new
    Closes #27

--- test/cpp/grammar.cpp
+++ test/cpp/grammar.cpp
@@ -6,6 +6,7 @@
 
 [type void] [meta X::][def Y]() {
   [variable x_]->[callee&property Y]([variable&callee A]([keyword this], [operator &][meta B::][qualified&variable C]<[type D]>));
+  [keyword delete] [[]] [variable baz];
 }
 
 [keyword class] [def Z] {
