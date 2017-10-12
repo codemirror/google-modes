b9836288857ab5854694112b228059463fce1b58
Marijn Haverbeke <marijn@haverbeke.nl>
2017-10-12 09:45:07 +0200

    [javascript mode] Fix tokenization of !=
    Issue #57

--- test/js/locals.js
+++ test/js/locals.js
@@ -1,1 +1,8 @@
-[keyword function] [def foo]([def a], [def b]) { [keyword var] [def c] [operator =] [number 10]; [keyword return] [variable-2 a] [operator +] [variable-2 c] [operator +] [variable d]; }
+[keyword function] [def foo]([def a], [def b]) {
+  [keyword var] [def c] [operator =] [number 10]; [keyword return] [variable-2 a] [operator +] [variable-2 c] [operator +] [variable d];
+
+  [keyword if] ([number 1] [operator !=] [number 2])
+    [keyword return] [atom false];
+
+  [variable-2 c][operator ++];
+}
