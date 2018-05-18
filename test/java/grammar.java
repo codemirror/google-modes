359b3fac882097e3ba9e60a5e88aaa9ab453ade6
Marijn Haverbeke <marijn@haverbeke.nl>
2018-05-18 10:33:05 +0200

    [javadoc grammar] Don't style leading whitespace and asterisk in braced groups
    Closes #147

--- test/java/grammar.java
+++ test/java/grammar.java
@@ -6,7 +6,8 @@
 [comment /** multi]
 [comment  @<i>N</i>]
 [comment  * line]
-[comment  * {][comment&tag @code][comment  ][comment&attribute some @code{with braces}][comment }]
+[comment  * {][comment&tag @code][comment  ][comment&attribute some @code{with braces}]
+[comment  * ][comment&attribute continued code][comment }]
 [comment  * comment ][comment&tag @tag][comment */]
 
 [meta @Foo](x = [string "something"])
