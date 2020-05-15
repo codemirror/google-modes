65941bc035f931b57066aecf03a13e9a4f4cf1a8
Sergei Lebedev <slebedev@google.com>
2020-05-15 20:56:59 +0100

    Added a few more tests including a case with :=
    Strictly speaking f"{x := 42:42d}" is an invalid f-string, but it doesn't
    mean we shouldn't highlight it properly.

--- test/py/fstrings.py
+++ test/py/fstrings.py
@@ -3,7 +3,7 @@
 [string f"foo bar"]
 [string f'''foo bar''']
 [string f"""foo bar"""]
-[comment # TODO(slebedev): '\'\' doesn't work]
+[comment # TODO(slebedev): Multi-line strings don't work.]
 
 [comment # f- with other prefixes]
 [string fr'foo bar']
@@ -18,8 +18,21 @@
 [comment # replacement with a conversion]
 [string f'foo ][operator {][variable bar][operator !][keyword s][operator }][string ']
 
+[comment # replacement with an invalid conversion]
+[string f'foo ][operator {][variable bar][operator !][keyword boo][operator }][string ']
+[string f'foo ][operator {][variable bar][operator !][operator }][string ']
+
 [comment # replacement with a format spec]
 [string f'foo ][operator {][variable bar][operator :][string 42d][operator }][string ']
 
 [comment # replacement with a nested format spec]
 [string f'foo ][operator {][variable bar][operator :][operator {][variable boo][operator }][string d][operator }][string ']
+
+[comment # replacement with both a conversion and a format spec]
+[string f'foo ][operator {][variable bar][operator !][keyword s][operator :][string 42d][operator }][string ']
+
+[comment # replacement with a misplaced format spec]
+[string f'foo ][operator {][variable bar][operator :][string 42d!s][operator }][string ']
+
+[comment # replacement with a walrus operator]
+[string f'foo ][operator {][variable bar][operator :=][number 42][operator :][string 42d][operator }][string ']
